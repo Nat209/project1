8dd0ebde14a3120e7a740d668f824eae31b261cd
dancamilo <131489732+crincon1@users.noreply.github.com>
2024-11-12 11:54:09 -0500

    .

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CfooterComponent } from './cfooter/cfooter.component';
+import { CheaderComponent } from './cheader/cheader.component';
+import { CformComponent } from './cform/cform.component';
+import { CcourseComponent } from './ccourse/ccourse.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CfooterComponent,
+    CheaderComponent,
+    CformComponent,
+    CcourseComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ccourse/ccourse.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ccourse',
+  templateUrl: './ccourse.component.html',
+  styleUrl: './ccourse.component.css'
+})
+export class CcourseComponent {
+//declaracion de propiedades, atributos o variables
+ private courses!:string[];
+ private response:string|boolean; //Union de tipos 
+
+ tittle:string="FORMULARIO DE REGISTRO"
+
+constructor(){
+  this.courses=['JAVA', 'JAVASCRIPT', 'PYTHON'];
+  this.response="";
+}
+
+//metoddos
+public getCourses():string[]{
+  return this.courses;
+}
+public getResponse():string|boolean{
+  return this.response;
+}
+
+
+private registerCourse(name:string, age:string, course:string){
+  return this.response=`Se ha inscrito en el curso: ${course} el estudiante: ${name} de edad: ${age}`;
+}
+
+public getRegisterCourse(name:string, age:string, course:string){
+   this.registerCourse(name, age, course);
+}
+
+
+}
